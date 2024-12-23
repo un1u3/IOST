337d374bbbb28117111505f409151cc07b98c816
Arun Khanal <zarunkhanal1@gmail.com>
2024-12-23 22:25:25 +0545

    Structure of C program explained in detail

---
+++ C/1.structure.of.c.program.c
@@ -0,0 +1,26 @@
+//? A simple program to print hello world using printf function.
+//? This program is to learn about the syntax of C programming
+
+// the above given text is an example of comment in C and is called block comments.
+// Comments are of two main types in c "//" which is block comment or single line comment that means it only comments the line
+/* wheras there is another comment in C which is /* --commment here-- */ /* which is scope comment that means it comments the scope where the whole comments is there*/
+
+// above given comments is documentation section
+
+#include <stdio.h> // this is preprocessor section
+#define pi 3.1415  // definition section
+int a = 25;        // Global declaration section
+
+int main()
+{ // main section
+}
+
+// the bottom given blocks of codes are subPrograms
+
+int sum()
+{
+}
+
+int factorial()
+{
+}
