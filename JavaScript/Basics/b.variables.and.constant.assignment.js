9f6256727a6c6b820653329dca0b08eb8cd27b19
Arun Khanal <zarunkhanal1@gmail.com>
2025-01-03 22:50:20 +0545

    a bit more basics

---
+++ JavaScript/Basics/b.variables.and.constant.assignment.js
@@ -0,0 +1,39 @@
+//? data types
+// ? string
+// ? number
+// ? boolean
+// ? undefined
+// ? null
+// ? object
+
+// js => dynamic language => weakly typed
+// js => not compiler based
+// js => interpreter
+
+// ?string  => "", '', ``
+// let x = "hello";
+
+// console.log(typeof x);
+
+// ?number
+// let x = 123.12;
+
+// console.log(typeof x);
+
+// ? boolean =>  true or false
+
+// let x = false;
+// console.log(typeof x);
+
+// ? undefined
+// let x;
+// console.log(typeof x);
+
+//? null => object => historical bug
+// ? developer intentionally sets empty
+
+// let x = null;
+// console.log(typeof x); //object
+
+// let user = null;
+// console.log(typeof user);
